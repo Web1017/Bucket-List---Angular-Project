0ee96beba79089d76b7d860c0eacc93afda96cab
Javid <j-n_01@hotmail.com>
2017-12-06 16:45:45 -0500

    Added animations + modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 // import {MaterialModule} from './material.module';
 
+
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+//Router is Missing
 
 @NgModule({
   declarations: [
@@ -17,7 +19,8 @@ import { AboutComponent } from './about/about.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    
     // MaterialModule
   ],
   providers: [],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger,style,transition,animate,keyframes,query,stagger} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+      trigger('goals', [
+          transition('* => *', [
+            query(':enter', style ({ opacity: 0}), {optional: true}),
+
+
+            query(':enter', stagger('300ms', [
+              animate('.6s ease-in', keyframes ([
+                style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+                style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+                style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+              ]))]),{optional: true}),
+
+
+            query(':leave', stagger('300ms', [
+                            animate('.6s ease-in', keyframes ([
+                              style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+                              style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+                              style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
+                            ]))]),{optional: true})
+          ])
+      ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
@@ -14,7 +38,7 @@ export class HomeComponent implements OnInit {
 
     goalTxt: string = "My Initial Goal!";
 
-    goals = [];
+    goals = ["Travel the World","Become an artist","Become a famous actor"];
 
   constructor() { }
 
@@ -28,6 +52,10 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalTxt);
     this.goalTxt = '';
     this.itemCount = this.goals.length;
+  }
+
+  removeItem(i) {
+    this.goals.splice(i, 1);
   }
 
 }
